193de5ed08e92920a591e1ead7ec83c8e092d181
Sebastian <saad11012002@gmail.com>
2023-11-27 20:51:26 -0300

    Avance en template index, view index y style.css

--- miapp/views.py
+++ miapp/views.py
@@ -2,11 +2,58 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+
+from miapp.models import Evento
+
+
 # Create your views here.
 def index(request):
-    data ={
+    segmento = request.GET.get("segmento", "S")
+    tipo = request.GET.get("tipo", "S")
+    año = 2023
+
+    if(segmento == "S"):
+        opciones_segmentos = [('S', "Todos")] + list(Evento.SEGMENTO_CHOICES)
+    else:
+        opciones_segmentos = list(Evento.SEGMENTO_CHOICES)
+        for opcion in opciones_segmentos:
+            if opcion[0] == segmento:
+                opciones_segmentos.remove(opcion)
+                opciones_segmentos.insert(0, opcion)
+        opciones_segmentos.append(('S', "Todos"))
+
+    if(tipo == "S"):
+        opciones_tipo = [('S', "Todos" )] + list(Evento.TIPO_CHOICES)
+    else:
+        opciones_tipo = list(Evento.TIPO_CHOICES)
+        for opcion in opciones_tipo:
+            if opcion[0] == tipo:
+                opciones_tipo.remove(opcion)
+                opciones_tipo.insert(0, opcion)
+        opciones_tipo.append(('S', "Todos"))
+    
+   
+    
+
+    eventos = Evento.objects.all()
+    if (segmento != "S"):
+        eventos = eventos.filter(segmento = segmento)
+    if (tipo != "S"):
+        eventos = eventos.filter(tipo = tipo)
+
+
+    data ={ 
+        "año": año,
+        "opciones_segmentos": opciones_segmentos,
+        "opciones_tipo": opciones_tipo,
+        "eventos": eventos,
+        "segmento": segmento
     }
     return render(request,'miapp/index.html',data)
+
+
+
+
 
 def singup(request):
 
